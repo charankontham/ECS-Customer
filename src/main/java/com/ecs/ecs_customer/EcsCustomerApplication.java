2da8f8f8e74a354ddaefc5772eb3cc2e951227e7
= <=>
2024-09-15 05:45:42 -0400

    Initial push for project files

---
+++ src/main/java/com/ecs/ecs_customer/EcsCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.ecs.ecs_customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsCustomerApplication.class, args);
+	}
+
+}
